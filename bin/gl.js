4a460f09d078fd69aac23c15e91d9f6a5c141c35
zengqinglong <zengqinglong@buffalo-robot.com>
2022-06-02 16:58:01 +0800

    完成

---
+++ bin/gl.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+
+const { showLog } = require("../main");
+
+const dir = "E:/work/device-manager-web";
+showLog(...process.argv.slice(2));
--- main.js
+++ main.js
@@ -1,5 +1,55 @@
-const shelljs = require('shelljs');
+const shelljs = require("shelljs");
+const dayjs = require("dayjs");
+const path = require("path");
 
-const ret = shelljs.exec(`git log --after="2018-08-21 00:00:00"  --date=format:'%Y-%m-%d' `);
+const showLog = (dayCount = 7) => {
+  const date = dayjs()
+    .startOf("D")
+    .subtract(dayCount - 1, "day")
+    .format("YYYY-MM-DD HH:mm:ss");
 
-console.log(ret)
+  const ret = shelljs.exec(
+    'git log --after="' +
+      date +
+      '"  --date=format:"%Y-%m-%d" --pretty=format:" %C(yellow)%d%Cblue %s %Cgreen(%cd) %C(bold blue)%Creset"'
+  );
+
+  if (ret.code === 0) {
+    const data = ret.stdout;
+
+    const lines = data
+      .split("\n")
+      .reverse()
+      .filter(Boolean)
+      .map((i) => ({
+        time: i.slice(-13).trim(),
+        text: i
+          .slice(0, -13)
+          .trim()
+          .replace(/^(\(.*?\))\s*/, ""),
+      }));
+
+    if (lines.length === 0) {
+      console.log(`最近${dayCount}天没有记录`);
+      return;
+    }
+
+    const group = {};
+    lines.forEach(({ time, text }) =>
+      (group[time] || (group[time] = [])).push(text)
+    );
+
+    let str = "\n\n";
+
+    for (let key in group) {
+      str += key + "\n";
+      str += group[key].join("\n") + "\n\n";
+    }
+
+    console.log(str);
+  }
+};
+
+module.exports = {
+  showLog,
+};
